80cde5c2c319dc7788aa8d8a67204a4fbe175ecb
shkip <Nikitakrut.2110@gmail.com>
2023-06-10 02:12:07 +0300

    w

---
+++ src/layout/footer.jsx
@@ -0,0 +1,12 @@
+function Footer(){
+    return <footer className="page-footer Purpule">
+
+          <div className="footer-copyright">
+            <div className="container">
+            © 2023 Salcev N
+            <a class="grey-text text-lighten-4 right" href="#!">Repository</a>
+            </div>
+          </div>
+    </footer>
+}
+export { Footer}; 
